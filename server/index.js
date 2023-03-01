33591c06dcf2465dbf5e1296bae5f543bf6a592a
JOSEPH <lena.boya26@gmail.com>
2023-03-01 16:59:32 +0100

    ejs

--- server/index.js
+++ server/index.js
@@ -3,72 +3,69 @@ import express from 'express';
 import { google } from 'googleapis';
 
 
-
-
 dotenv.config();
 const app = express();
 
 
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 
-app.use((_req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+
+app.post('/', async (req, res) => {
+  const { name, request } = req.body;
+  const auth = new google.auth.GoogleAuth({
+    keyFile: 'credentials.json',
+    scopes: 'https://www.googleapis.com/auth/spreadsheets',
   });
   
+  // Create client instance for auth
+  const client = await auth.getClient();
 
-app.post('/FormComponent', async (req, res) => {
-  
-    const auth = new google.auth.GoogleAuth({
-        keyFile: 'credentials.json',
-        scopes: 'https://www.googleapis.com/auth/spreadsheets',
-    });
-    
-    // Create client instance for auth
-    const client = await auth.getClient();
+  // Instance of Google Sheets API
+  const googleSheets = google.sheets({ version: 'v4', auth: client });
 
-    // Instance of Google Sheets API
-    const googleSheets = google.sheets({ version: 'v4', auth: client });
+  // Spreadsheet ID
+  const spreadsheetID = process.env.SPREADSHEET_ID;
 
-    // spreadsheet id
-    
-    const spreadsheetID = process.env.SPREADSHEET_ID;
+  // Get metadata about spreadsheet
+  const metaData = await googleSheets.spreadsheets.get({
+    auth,
+    spreadsheetId: spreadsheetID,
+  });
 
 
-    //get metadata about spreadsheet
-    const metaData = await googleSheets.spreadsheets.get({
-        auth,
-        spreadsheetId: spreadsheetID,
-    });
-
-    const getRows = await googleSheets.spreadsheets.values.get({
-        auth,
-        spreadsheetId: spreadsheetID,
-        range: 'SAISIE CLIENT!E5',
-       
-
-    });
+  const getRows = await googleSheets.spreadsheets.values.get({
+   auth,
+   spreadsheetId: spreadsheetID,
+   range: 'SAISIE CLIENT!E5',
+ });
 
 
 
+  // Append the values to the next empty row
+  await googleSheets.spreadsheets.values.append({
+    auth,
+    spreadsheetId: spreadsheetID,
+    range: 'SAISIE CLIENT!E5',
+    valueInputOption: 'USER_ENTERED',
+    resource: {
+      values: [
+        [name, request],
+      
 
-    await googleSheets.spreadsheets.values.append({
-        auth,
-        spreadsheetId: spreadsheetID,
-        range: 'SAISIE CLIENT!E5' ,
-        valueInputOption: 'USER_ENTERED',
-        resource: {
-            values: [
-                ['value1'],
-            ]
-        },
-       
-    });
-   
+      ]
+    },
+  });
 
-
-    res.send(getRows.data.values);
+  res.send("Success");
 
 });
+
+
 
 app.listen(5000, () => console.log('Listening on port 5000'));
